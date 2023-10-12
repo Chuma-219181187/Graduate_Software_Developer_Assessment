3154cf9c1eb68306031d0c975d25b91b57f3bb93
NXAZONKEC <2191811857@mycput.ac.za>
2023-10-12 12:38:38 +0200

    files added

---
+++ src/main/java/com/enviro/assessment/grad001/ChumaNxazonke/GraduateSoftwareDeveloperAssessment/Graduate_Software_Developer_Assessment/GraduateSoftwareDeveloperAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.ChumaNxazonke.GraduateSoftwareDeveloperAssessment.Graduate_Software_Developer_Assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduateSoftwareDeveloperAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduateSoftwareDeveloperAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/ChumaNxazonke/GraduateSoftwareDeveloperAssessment/Graduate_Software_Developer_Assessment/GraduateSoftwareDeveloperAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.ChumaNxazonke.GraduateSoftwareDeveloperAssessment.Graduate_Software_Developer_Assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateSoftwareDeveloperAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
